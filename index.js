f0154a14c82f46922881c219b78a044d3d8748d4
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-04-07 14:18:47 -0500

    commit tree 89f8a387640dc6ccd2c1aac775488cb101110132

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+names = ['bob','sandy']
+console.log(writeCards(names,'birthday'))
+
+function writeCards(array, event){
+    let messages=[]
+    for (let i = 0; i<array.length; i++){
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+        debugger;
+    }
+    return messages
+}
+
+function countDown(n){
+    while (n>=0)
+    {console.log(n--)}
+}
